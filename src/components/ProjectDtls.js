12ca9895d0f0e23d1da0a38d3d60e2aea2e7d44e
Mr-R-e-X <souvikhazra151@gmail.com>
2024-06-28 15:21:41 +0530

    done

--- src/components/ProjectDtls.js
+++ src/components/ProjectDtls.js
@@ -3,8 +3,8 @@ import React from "react";
 const ProjectDtls = ({ name, desc }) => {
   return (
     <div className="ns-wrapper">
-      <h1 data-ns-test={name}> {name} </h1>
-      <h6 data-ns-test={name}> {desc} </h6>
+      <h1 data-ns-test="project-name"> {name} </h1>
+      <h6 data-ns-test="project-description"> {desc} </h6>
     </div>
   );
 };
